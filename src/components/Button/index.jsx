d7490e99c4c8af614b5963e7f99e676ade1d916b
matheustkaczyk <matheus111904@hotmail.com>
2022-04-18 09:26:35 -0300

    Componentizando Button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+export const Button = ({ type, classN, handleClick, text, value }) => {
+  return(
+    <button
+      type={type}
+      className={classN}
+      onClick={handleClick(value)}
+    >
+      {text}
+    </button>
+  )
+}
